2a5e970e4ff555c72450e390b5e9022cc81e40fc
kangrack333@gmail.com <kangrack333@gmail.com>
2023-03-18 17:39:40 +0900

    23-03-18 Spring Commit

---
+++ src/test/java/com/knight/hellosping/HelloSpingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.knight.hellosping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

72d0f3a3572c828a3666aca63b8842a243614546
kangrack333@gmail.com <kangrack333@gmail.com>
2023-03-20 12:19:31 +0900

    23-03-20 Spring Commit

--- src/main/java/com/knight/hellosping/controller/HelloController.java
+++ src/main/java/com/knight/hellosping/controller/HelloController.java
@@ -1,18 +1,14 @@
 package com.knight.hellosping.controller;
 
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HelloController {
-
   @GetMapping("hello")
-  public String hello(Model model){
-    model.addAttribute("date","hello!!");
+  public String hello(Model model) {
+    model.addAttribute("data", "Yo!!");
     return "hello";
-
   }
-
 }
